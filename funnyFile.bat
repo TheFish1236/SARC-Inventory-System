e1656103b7bf10bb889d9553b925341ef2b37450
Fish <muhyi.siddiqui@ucf.edu>
2026-06-22 13:59:55 -0400

    added launcher for script for ease of access

---
+++ funnyFile.bat
@@ -0,0 +1,10 @@
+@echo off
+TITLE SARC Inventory Kiosk
+COLOR 0A
+
+echo Starting SARC Inventory System...
+echo =================================
+echo Launching Tracker...
+python tracker.py
+
+pause
